1d65782cf76a55ddec8a5c46ddd60d321ad2ccfe
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-02-15 06:11:14 +0900

    ターンの推移を管理するTurnManagerを作成した

--- Assets/Script/Kasai/EnemyController.cs
+++ Assets/Script/Kasai/EnemyController.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    /// <summary> 敵の体力/// </summary>
+    /// <summary> 敵の体力 </summary>
     [SerializeField] int m_hp = 10;
-    /// <summary> 敵の落とす素材/// </summary>
+    /// <summary> 敵の落とす素材 </summary>
     [SerializeField] GameObject m_drop = null;
+    /// <summary> 敵の動く速度 </summary>
+    [SerializeField] int m_speed = 10;
+    /// <summary>  </summary>
+    public bool attackFlag = false;
+    TurnManager turnManager;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +22,14 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (turnManager.turnName == TurnManager.TurnName.MOVE)
+        {
+
+        }
+        else if (turnManager.turnName == TurnManager.TurnName.ATTACK)
+        {
+            Hit();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -34,11 +46,22 @@ public class EnemyController : MonoBehaviour
     }
 
     /// <summary>
-    /// 敵がダメージを受けたときに呼ばれる関数
+    /// 敵が攻撃を受けたときに呼ばれる関数
     /// </summary>
+    /// <param name="power">プレイヤーの攻撃力</param>
     public void Damage(int power)
     {
         m_hp -= power;
-        //Debug.Log($"敵にpowerのダメージを与えた！");
+        Debug.Log("敵に" + power + "のダメージを与えた！");
+        if (m_hp == 0)
+        {
+            Instantiate(m_drop, this.transform.position, this.transform.rotation);
+            Debug.Log("敵に" + power + "のダメージを与えた！");
+        }
+    }
+
+    public void Move()
+    {
+
     }
 }
---
+++ Assets/Script/Kasai/TurnManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour
+{
+    /// <summary> ターン内の状態を表す</summary>
+    public TurnName turnName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// ターン内の状態を表す
+    /// </summary>
+    public enum TurnName
+    {
+        MOVE,
+        ATTACK,
+        ITEMMAKE,
+    }
+}
