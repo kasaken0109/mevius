e2d4496411182d790db21632fa144e81c52495ff
sorude26 <sorude26@gmail.com>
2021-02-14 15:05:29 +0900

    プレイヤー移動仮作成

--- Assets/Script/Takeuchi/Player.cs
+++ Assets/Script/Takeuchi/Player.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /// <summary>プレイヤーX座標初期位置</summary>
+    [SerializeField] private int startPosX;
+    /// <summary>プレイヤーY座標初期位置</summary>
+    [SerializeField] private int startPosZ;
+    /// <summary>プレイヤーX座標現在地</summary>
+    public int CurrentPosX { get; protected set; }
+    /// <summary>プレイヤーY座標現在地</summary>
+    public int CurrentPosY { get; protected set; }
+    private float squaresSize;
+    private float positionUpdateTimer;
+    private void Awake()
+    {
+        CurrentPosX = startPosX;
+        CurrentPosY = startPosZ;
+    }
     void Start()
     {
-        
+        squaresSize = MapBase.Instance.GetSquaresSize();
+        transform.position = new Vector2(CurrentPosX * squaresSize, CurrentPosY * squaresSize);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (Input.GetAxisRaw("Horizontal") != 0 && positionUpdateTimer <= 0 || Input.GetAxisRaw("Vertical") != 0 && positionUpdateTimer <= 0)
+        {
+            positionUpdateTimer = 0.5f;
+            if (Input.GetAxisRaw("Horizontal") > 0 )
+            {
+                CurrentPosX++;
+            }
+            else if (Input.GetAxisRaw("Horizontal") < 0 )
+            {
+                CurrentPosX--;
+            }
+            else if (Input.GetAxisRaw("Vertical") > 0)
+            {
+                CurrentPosY++;
+            }
+            else if (Input.GetAxisRaw("Vertical") < 0)
+            {
+                CurrentPosY--;
+            }
+            transform.position = new Vector2(CurrentPosX * squaresSize, CurrentPosY * squaresSize);
+        }
         
+        if (positionUpdateTimer > 0)
+        {
+            positionUpdateTimer -= Time.deltaTime;            
+        }
     }
 }
