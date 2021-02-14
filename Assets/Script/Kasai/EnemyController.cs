0bdcccc0712c706f74184fce40d96b46030a955a
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-02-14 16:05:11 +0900

    EnemyControllerに関数を追加した

--- Assets/Script/Kasai/EnemyController.cs
+++ Assets/Script/Kasai/EnemyController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    /// <summary> 敵の体力/// </summary>
+    [SerializeField] int m_hp = 10;
+    /// <summary> 敵の落とす素材/// </summary>
+    [SerializeField] GameObject m_drop = null;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,27 @@ public class EnemyController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+    /// <summary>
+    /// 敵が攻撃をする関数
+    /// </summary>
+    public void Hit()
+    {
+
+    }
+
+    /// <summary>
+    /// 敵がダメージを受けたときに呼ばれる関数
+    /// </summary>
+    public void Damage(int power)
+    {
+        m_hp -= power;
+        //Debug.Log($"敵にpowerのダメージを与えた！");
     }
 }
