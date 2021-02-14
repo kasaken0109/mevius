a677994e3c31e91d8c6bce8d78a23f97994e9e1c
sorude26 <sorude26@gmail.com>
2021-02-14 17:02:00 +0900

    移動方向をenumで管理に変更、コメント追加

--- Assets/Script/Takeuchi/Player.cs
+++ Assets/Script/Takeuchi/Player.cs
@@ -12,12 +12,20 @@ public class Player : MonoBehaviour
     public int CurrentPosX { get; protected set; }
     /// <summary>プレイヤーY座標現在地</summary>
     public int CurrentPosY { get; protected set; }
+    /// <summary> 移動速度 </summary>
+    [SerializeField] private float moveSpeed = 1.0f;
     private float squaresSize;
     private float positionUpdate;
-    [SerializeField]
-    private float moveSpeed = 1.0f;
-    private bool ud;
-    private bool lr;
+    private float inputTimer;
+    private enum MoveAngle
+    {
+        Up,
+        Down,
+        Left,
+        Right,
+    }
+    /// <summary> 移動方向 </summary>
+    [SerializeField] private MoveAngle angle = MoveAngle.Down;
     private void Awake()
     {
         CurrentPosX = startPosX;
@@ -31,15 +39,15 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetAxisRaw("Horizontal") != 0 && positionUpdate == 0 || Input.GetAxisRaw("Vertical") != 0 && positionUpdate == 0)
+        //入力があれば移動、
+        if (Input.GetAxisRaw("Horizontal") != 0 && positionUpdate == 0 && inputTimer <= 0 || Input.GetAxisRaw("Vertical") != 0 && positionUpdate == 0 && inputTimer <= 0)
         {            
             if (Input.GetAxisRaw("Horizontal") > 0 )
             {
                 if (!MapBase.Instance.GetSquaresNoIntrusion(CurrentPosX+1, CurrentPosY))
                 {
                     CurrentPosX++;
-                    ud = true;
-                    lr = true;
+                    angle = MoveAngle.Right;
                     positionUpdate = 1.0f;
                 }
             }
@@ -48,8 +56,7 @@ public class Player : MonoBehaviour
                 if (!MapBase.Instance.GetSquaresNoIntrusion(CurrentPosX-1, CurrentPosY))
                 {
                     CurrentPosX--;
-                    ud = false;
-                    lr = false;
+                    angle = MoveAngle.Left;
                     positionUpdate = 1.0f;
                 }
             }
@@ -58,8 +65,7 @@ public class Player : MonoBehaviour
                 if (!MapBase.Instance.GetSquaresNoIntrusion(CurrentPosX, CurrentPosY + 1))
                 {
                     CurrentPosY++;
-                    ud = true;
-                    lr = false;
+                    angle = MoveAngle.Up;
                     positionUpdate = 1.0f;
                 }
             }
@@ -68,12 +74,14 @@ public class Player : MonoBehaviour
                 if (!MapBase.Instance.GetSquaresNoIntrusion(CurrentPosX, CurrentPosY - 1))
                 {
                     CurrentPosY--;
-                    ud = false;
-                    lr = true;
+                    angle = MoveAngle.Down;
                     positionUpdate = 1.0f;
                 }
             }
+            //ここでアイテムがあればターン終了時に取得
+            inputTimer = 0.5f;
         }        
+        //移動した際の処理
         if (positionUpdate > 0)
         {
             positionUpdate -= moveSpeed * Time.deltaTime;
@@ -82,22 +90,27 @@ public class Player : MonoBehaviour
                 positionUpdate = 0;
                 //ここでターン終了処理呼び出し
             }
-            if (ud && !lr)
+            switch (angle)
             {
-                transform.position = new Vector2(CurrentPosX * squaresSize, (CurrentPosY - positionUpdate) * squaresSize);
+                case MoveAngle.Up:
+                    transform.position = new Vector2(CurrentPosX * squaresSize, (CurrentPosY - positionUpdate) * squaresSize);
+                    break;
+                case MoveAngle.Down:
+                    transform.position = new Vector2(CurrentPosX * squaresSize, (CurrentPosY + positionUpdate) * squaresSize);
+                    break;
+                case MoveAngle.Left:
+                    transform.position = new Vector2((CurrentPosX + positionUpdate) * squaresSize, CurrentPosY * squaresSize);
+                    break;
+                case MoveAngle.Right:
+                    transform.position = new Vector2((CurrentPosX - positionUpdate) * squaresSize, CurrentPosY * squaresSize);
+                    break;
+                default:
+                    break;
             }
-            else if (!ud && lr)
-            {
-                transform.position = new Vector2(CurrentPosX * squaresSize, (CurrentPosY + positionUpdate) * squaresSize);
-            }
-            else if (ud && lr)
-            {
-                transform.position = new Vector2((CurrentPosX - positionUpdate) * squaresSize, CurrentPosY * squaresSize);
-            }
-            else if (!ud && !lr)
-            {
-                transform.position = new Vector2((CurrentPosX + positionUpdate) * squaresSize, CurrentPosY * squaresSize);
-            }
+        }
+        if (inputTimer > 0)
+        {
+            inputTimer -= Time.deltaTime;
         }
     }
 }
