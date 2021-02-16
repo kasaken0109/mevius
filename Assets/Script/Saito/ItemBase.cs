776e2cbaa94015c10ab22c3d48c66781ccfb10c9
saitoSay <seinyacchi@gmail.com>
2021-02-16 13:26:48 +0900

    ItemManagerにアイテムを取得する関数の追加

--- Assets/Script/Saito/ItemBase.cs
+++ Assets/Script/Saito/ItemBase.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class ItemBase : MonoBehaviour
 {
+    public int posX;
+    public int posY;
+    private float squaresSize;
+    private void Start()
+    {
+        squaresSize = MapBase.Instance.GetSquaresSize();
+        transform.position = new Vector2(posX * squaresSize, posY * squaresSize);
+    }
+    
     /// <summary> 素材の種類 </summary>
     public enum RecycleMaterial
     {
--- Assets/Script/Saito/ItemManager.cs
+++ Assets/Script/Saito/ItemManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class ItemManager : MonoBehaviour
 {
@@ -8,5 +9,35 @@ public class ItemManager : MonoBehaviour
     public int woodNumber = 0;
     /// <summary> 鉄の所持数 </summary>
     public int metalNumber = 0;
+    private GameObject[] items;
+
+    private void Start()
+    {
+        
+    }
+    public void SetItem()
+    {
+        GameObject player = GameObject.Find("Player");
+        items = GameObject.FindGameObjectsWithTag("Item");
+        foreach (var item in items)
+        {
+            if (item.transform.position.x == player.transform.position.x && item.transform.position.y == player.transform.position.y)
+            {
+                Debug.Log("e");
+                if (item.GetComponent<Wood>())
+                {
+                    woodNumber++;
+                    Destroy(item);
+                }
+                if (item.GetComponent<Metal>())
+                {
+                    metalNumber++;
+                    Destroy(item);
+                }
+            }
+            Debug.Log(player.transform.position);
+        }
+        
+    }
     
 }
