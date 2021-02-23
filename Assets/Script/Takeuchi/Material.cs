e78fd58333088f4a1286410b8a25f0ff78ab1576
sorude26 <sorude26@gmail.com>
2021-02-23 10:39:47 +0900

    素材に仮移動関数を作成した

--- Assets/Script/Takeuchi/Material.cs
+++ Assets/Script/Takeuchi/Material.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Material : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
+    private float startMoveTimer;
+    [SerializeField]
+    private float moveSpeed = 10.0f;
     void Update()
     {
-        
+        if (startMoveTimer > 0)
+        {
+            startMoveTimer -= Time.deltaTime;
+            transform.localPosition = new Vector2(0, transform.localPosition.y + moveSpeed); 
+        }
+        else
+        {
+            
+        }
+    }
+    
+    public void StartMove(Quaternion angel)
+    {
+        transform.rotation = angel;
+        startMoveTimer = 1.0f;
     }
 }
