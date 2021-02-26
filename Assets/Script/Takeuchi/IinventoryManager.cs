6236da1c0dc014d35cf454658c25fb5a3b79e648
sorude26 <sorude26@gmail.com>
2021-02-26 19:01:48 +0900

    アイテムのインベントリー管理クラスを追加した

---
+++ Assets/Script/Takeuchi/IinventoryManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IinventoryManager : MonoBehaviour
+{
+    List<ItemBase> haveItems;
+    private void Awake()
+    {
+        haveItems = new List<ItemBase>();
+    }
+
+    public void AddItem(ItemBase item)
+    {
+        haveItems.Add(item);
+    }
+
+}
