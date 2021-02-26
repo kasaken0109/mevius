62a67fe5765239b063ecd6a2bb9705982682aede
sorude26 <sorude26@gmail.com>
2021-02-26 19:03:12 +0900

    テスト用アイテムの基底クラスを作成した

---
+++ Assets/Script/Takeuchi/Items/ItemBaseData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Items", menuName = "Items/Item")]
+public class ItemBaseData : ScriptableObject
+{
+    public string ItemName;
+    public Sprite ItemImage;
+}
+
