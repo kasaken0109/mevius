8b2178fd4c740cd4e50fb014c6a0887405334336
sorude26 <sorude26@gmail.com>
2021-02-26 20:11:38 +0900

    IinventoryGridにアイテムスロット表示機能を追加した

--- Assets/Script/Takeuchi/InventoryGrid.cs
+++ Assets/Script/Takeuchi/InventoryGrid.cs
@@ -1,18 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class InventoryGrid : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject itemSlotPrefab;
+    [SerializeField] int slotNumgber = 8;
+    [SerializeField] ItemBaseData[] AllItems;
+    public List<ItemSlot> ItemSlots { get; private set; }
     void Start()
     {
-        
+        ItemSlots = new List<ItemSlot>();
+        for (int i = 0; i < slotNumgber; i++)
+        {
+            GameObject slot = Instantiate(itemSlotPrefab, this.transform);
+            ItemSlot slotData = slot.GetComponent<ItemSlot>();
+            if (i < AllItems.Length)
+            {
+                slotData.SetItem(AllItems[i]);
+            }
+            else
+            {
+                slotData.SetItem(null);
+            }
+            ItemSlots.Add(slotData);
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public bool AddItemCheck()
     {
-        
+        foreach (var item in ItemSlots)
+        {
+            if (!item)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public void AddItem(ItemBaseData item)
+    {
+        ItemSlots.Where(i => i.Item == null).FirstOrDefault().SetItem(item);
     }
 }
