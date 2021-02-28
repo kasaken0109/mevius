475a38e5a13281b31211f663d5cb9fee8e0c1739
sorude26 <sorude26@gmail.com>
2021-02-28 18:42:54 +0900

    アイテム入手用コマンドスクリプトを追加した

---
+++ Assets/Script/Takeuchi/ItemGetCommand.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CommandType
+{
+    GetGarbage,
+    GetTool,
+}
+public class ItemGetCommand : MonoBehaviour
+{
+    [SerializeField]
+    CommandType command = CommandType.GetGarbage;
+
+    public void OnClickThis()
+    {
+        switch (command)
+        {
+            case CommandType.GetGarbage:
+                IinventoryManager.Instance.ItemGet(0);
+                break;
+            case CommandType.GetTool:
+                IinventoryManager.Instance.ItemGet(1);
+                break;
+            default:
+                break;
+        }
+    }
+}
