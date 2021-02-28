be7b102b4cacd645d7f16c9c4c7dcb205c0c0729
sorude26 <sorude26@gmail.com>
2021-02-28 16:59:17 +0900

    プレイヤーへのメッセージ表示機能スクリプトを追加した

---
+++ Assets/Script/Takeuchi/PlayerMessage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMessage : MonoBehaviour
+{
+    [SerializeField]
+    GameObject target;
+    RectTransform rect;
+    private void Start()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+    private void Update()
+    {
+        rect.position = RectTransformUtility.WorldToScreenPoint(Camera.main, target.transform.position);
+    }
+}
