3082d50784da2a704bdb144ca7c06b609a433770
sorude26 <sorude26@gmail.com>
2021-03-01 16:23:16 +0900

    シーン遷移機能を追加した

---
+++ Assets/Script/Takeuchi/SceneChangeControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChangeControl : MonoBehaviour
+{
+    static SceneChangeControl Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+    public static void SceneChange(int i)
+    {
+        switch (i)
+        {
+            case 0:
+                SceneManager.LoadScene("TakeutiTest3");
+                break;
+            case 1:
+                SceneManager.LoadScene("TakeutiTest4");
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/Script/Takeuchi/SceneChangePoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChangePoint : MonoBehaviour
+{
+    [SerializeField]
+    int target = 0;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.GetComponent<Player>();
+        if (player)
+        {
+            SceneChangeControl.SceneChange(target);
+        }
+    }
+}
