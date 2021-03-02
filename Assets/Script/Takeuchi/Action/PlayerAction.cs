0a6bf8f6f85a58547e6b7b37bcdb99a51bf2c94e
sorude26 <sorude26@gmail.com>
2021-03-02 17:34:09 +0900

    Playerの基本的な動きを作成した

--- Assets/Script/Takeuchi/Action/PlayerAction.cs
+++ Assets/Script/Takeuchi/Action/PlayerAction.cs
@@ -4,15 +4,93 @@ using UnityEngine;
 
 public class PlayerAction : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D m_rb;
+    [SerializeField]
+    float jumpPower = 1f;
+    [SerializeField]
+    float moveSpeed = 2f;
+    [SerializeField]
+    LayerMask groundLayer = 0;
+    [SerializeField]
+    Vector2 rayForGround = Vector2.down;
+    [SerializeField]
+    int maxJumpCount = 2;
+    int jumpCount = 0;
+    bool directionLR;
+    bool chang;
+    bool onGround;
     void Start()
     {
-        
+        m_rb = GetComponent<Rigidbody2D>();
+        directionLR = true;
+        jumpCount = maxJumpCount;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, rayForGround, rayForGround.magnitude, groundLayer);
+        if (hit.collider)
+        {
+            onGround = true;
+            jumpCount = maxJumpCount;
+        }
+        else
+        {
+            onGround = false;
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (onGround)
+            {
+                m_rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            }
+            else if (jumpCount > 0)
+            {
+                m_rb.velocity = Vector2.zero;
+                m_rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+                jumpCount--;
+            }
+        }
+    }
+    private void FixedUpdate()
     {
         
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            
+                Vector2 dir = Vector2.zero;
+                if (Input.GetAxisRaw("Horizontal") > 0)
+                {
+                    dir = Vector2.right * moveSpeed;
+                    if (!directionLR)
+                    {
+                        directionLR = true;
+                        chang = true;
+                    }
+                }
+                else if (Input.GetAxisRaw("Horizontal") < 0)
+                {
+                    dir = Vector2.right * -moveSpeed;
+                    if (directionLR)
+                    {
+                        directionLR = false;
+                        chang = true;
+                    }
+                }
+                dir.y = m_rb.velocity.y;
+                m_rb.velocity = dir;
+        }
+        if (chang)
+        {
+            if (directionLR)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+            else
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            chang = false;
+        }
     }
 }
