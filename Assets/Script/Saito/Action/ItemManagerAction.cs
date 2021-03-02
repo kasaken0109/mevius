d1b8524738d40328a2ad06fd9b5e8e3df6133ca7
saitoSay <seinyacchi@gmail.com>
2021-03-02 17:02:43 +0900

    アクション用のシーンとアイテムマネージャーの仮作成

---
+++ Assets/Script/Saito/Action/ItemManagerAction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManagerAction : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
