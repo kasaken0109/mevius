b215a225270a1d165e87e89865315b343a7be1c9
saitoSay <seinyacchi@gmail.com>
2021-03-03 21:34:32 +0900

    クリック時にアイテムがPlayerへ動く機能の追加をした

---
+++ Assets/Script/Saito/Action/Item.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField] GameObject m_player;
+    Rigidbody2D m_rb;
+    [SerializeField] float m_maxSpeed = 2f;
+    [SerializeField] float m_movePower = 10f;
+
+    public enum Materal
+    {
+        Kan,
+        Petbottle
+    }
+    [SerializeField] Materal type = Materal.Kan;
+
+    private void Start()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Vector2 playerVec = new Vector2(m_player.transform.position.x, m_player.transform.position.y);
+            m_rb.velocity = playerVec - (new Vector2(this.transform.position.x, this.transform.position.y));
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log(this.type.ToString());
+        }
+    }
+}
