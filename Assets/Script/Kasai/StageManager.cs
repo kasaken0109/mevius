6b7b253bb4a0e134c971973516d6bb2c2de3ccc2
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-03-05 15:07:55 +0900

    StageManagerを追加し、DeadZoneにPlayerが当たったときにSpawn地点に移動するように処理を追加した

---
+++ Assets/Script/Kasai/StageManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Transform m_spawn = null;
+    void Start()
+    {
+        m_spawn = GameObject.FindGameObjectWithTag("Spawn").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+            Instantiate(collision.gameObject, m_spawn.position,m_spawn.rotation);
+        }
+    }
+}
