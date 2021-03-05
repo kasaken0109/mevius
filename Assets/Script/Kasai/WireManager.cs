e3d6331f018a02a99e154d114268b25289b03d85
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-03-05 18:43:44 +0900

    ワイヤーの挙動を追加した

---
+++ Assets/Script/Kasai/WireManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WireManager : MonoBehaviour
+{
+    DistanceJoint2D m_joint;
+    LineRenderer m_line;
+    Collider2D m_hitObject = null;
+    [SerializeField] float m_jointDistance = 10f;
+    [SerializeField] float m_pullSpeed = 0.15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_joint = GetComponent<DistanceJoint2D>();
+        m_line = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_hitObject)
+        {
+            m_joint.enabled = true;
+            m_joint.connectedBody = m_hitObject.gameObject.GetComponent<Rigidbody2D>();
+            Debug.Log(m_joint.connectedBody);
+            m_joint.connectedAnchor = new Vector2(m_hitObject.transform.position.x, m_hitObject.transform.position.y);
+            m_joint.distance = Vector2.Distance(m_hitObject.transform.position, this.transform.position);
+            //m_joint.connectedAnchor = m_hitObject.transform.position;
+           
+        }
+
+        if (Input.GetButton("Fire1") && m_joint.distance >= m_jointDistance)
+        {
+            Debug.Log("巻き取り");
+            m_line.enabled = true;
+            DrawLaser(m_hitObject.gameObject.transform.position);
+            m_joint.distance -= m_pullSpeed;
+        }
+        else
+        {
+            Debug.Log("切り離し");
+            m_line.enabled = false;
+            m_joint.enabled = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Joint")
+        {
+            Debug.Log("入った！");
+            m_hitObject = collision;
+            Debug.Log(m_joint.distance);
+        }
+    }
+
+    void DrawLaser(Vector3 destination)
+    {
+        Vector3[] positions = { m_line.transform.position, destination };   // レーザーの始点は常に Muzzle にする
+        m_line.positionCount = positions.Length;   // Line を終点と始点のみに制限する
+        m_line.SetPositions(positions);
+    }
+}
