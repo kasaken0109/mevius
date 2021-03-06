39aa3ad58a2fd552322ad1058dad143d63633761
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-03-06 15:27:32 +0900

    弾生成時にプレイヤーの向いている向きによって弾の進行方向が変化するようにスクリプトを修正した

--- Assets/Script/Kasai/PlayerShootController.cs
+++ Assets/Script/Kasai/PlayerShootController.cs
@@ -12,16 +12,26 @@ public class PlayerShootController : MonoBehaviour
     [SerializeField] float m_bulletSpeed = 1f;
     /// <summary>弾の攻撃力</summary>
     [SerializeField] int m_bulletPower = 5;
+
+    PlayerAction player;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        if (player.directionLR)
+        {
+            rigidbody2D.velocity = new Vector2(1, 0) * m_bulletSpeed;
+        }
+        else
+        {
+            rigidbody2D.velocity = new Vector2(-1, 0) * m_bulletSpeed;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        rigidbody2D.velocity = new Vector2(1, 0) * m_bulletSpeed;
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
