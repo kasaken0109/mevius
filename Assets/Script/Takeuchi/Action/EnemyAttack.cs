519325fc1b0ba1d86a7bf0064e2dd4d802f347b2
sorude26 <sorude26@gmail.com>
2021-03-06 14:43:31 +0900

    敵の攻撃判定用スクリプトを追加した

---
+++ Assets/Script/Takeuchi/Action/EnemyAttack.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    private int power;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            PlayerAction.Instance.Damage(power,this.transform.position);
+        }
+    }
+    public void SetPower(int power)
+    {
+        this.power = power;
+    }
+}
