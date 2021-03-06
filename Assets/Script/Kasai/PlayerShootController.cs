7cfd821efa00aacfe3a957f0c1d26ec25f9c794f
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-03-06 14:28:02 +0900

    弾の処理に関するPlayerShootControllerを追加した

---
+++ Assets/Script/Kasai/PlayerShootController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 打ち出した弾に関する処理
+/// </summary>
+public class PlayerShootController : MonoBehaviour
+{
+    Rigidbody2D rigidbody2D;
+    /// <summary>弾の速度</summary>
+    [SerializeField] float m_bulletSpeed = 1f;
+    /// <summary>弾の攻撃力</summary>
+    [SerializeField] int m_bulletPower = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody2D.velocity = new Vector2(1, 0) * m_bulletSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<EnemyAction>().Damage(m_bulletPower);
+        }
+    }
+}
