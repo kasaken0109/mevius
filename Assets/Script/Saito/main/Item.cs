68d88e4c094d6fe759d2acc176b6af08a6209fc4
saitoSay <seinyacchi@gmail.com>
2021-03-11 01:27:11 +0900

    仮のItemDataを作成した

---
+++ Assets/Script/Saito/main/Item.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField]
+    Dictionary<string, ItemBaseData> itemList = new Dictionary<string, ItemBaseData>()
+    {
+        
+    };
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
